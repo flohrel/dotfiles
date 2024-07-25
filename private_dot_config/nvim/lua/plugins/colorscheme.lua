c51e836ca2ca8bd039e6839819bf9f42900b129a
Florian Ohrel <ohrel.florian@pm.me>
2024-07-25 18:01:09 +0200

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/chezmoi.lua Add .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/telescope/_extensions/find_files.lua Add .config/nvim/stylua.toml

---
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+if true then
+  return {}
+end
+return {
+  -- add Catppuccin
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
---
+++ private_dot_config/nvim/lua/telescope/_extensions/find_files.lua
@@ -0,0 +1,58 @@
+local chezmoi_commands = require("chezmoi.commands")
+local config = require("chezmoi").config
+
+local make_entry = require("telescope.make_entry")
+local telescope_config = require("telescope.config").values
+local action_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+
+local find = {}
+
+function find.execute(opts)
+  opts = opts or {}
+
+  local list = chezmoi_commands.list({
+    args = {
+      "--path-style",
+      "absolute",
+      "--include",
+      "files",
+      "--exclude",
+      "externals",
+    },
+  })
+
+  opts.cwd = os.getenv("HOME")
+
+  pickers
+    .new(opts, {
+      prompt_title = "Chezmoi Files",
+      finder = finders.new_table({
+        results = list,
+        entry_maker = make_entry.gen_from_file(opts),
+      }),
+      attach_mappings = function(prompt_bufnr, map)
+        local edit_action = function()
+          actions.close(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          chezmoi_commands.edit({
+            targets = selection.value,
+          })
+        end
+
+        for _, v in ipairs(config.telescope.select) do
+          map("i", v, "select_default")
+        end
+
+        actions.select_default:replace(edit_action)
+        return true
+      end,
+      previewer = telescope_config.file_previewer(opts),
+      sorter = telescope_config.generic_sorter(opts),
+    })
+    :find()
+end
+
+return find
