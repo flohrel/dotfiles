c125e67a500e658e77af6a313114226ec8185fb2
Florian Ohrel <ohrel.florian@pm.me>
2025-04-18 15:05:38 +0200

    Update .config/oh-my-zsh/aliases.zsh

--- private_dot_config/oh-my-zsh/aliases.zsh
+++ private_dot_config/oh-my-zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias vi="vim"
 alias ggc="gcc -Wall -Wextra -Werror"
 alias cm="chezmoi"
 alias pn="pnpm"
+
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
